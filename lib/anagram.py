467efa7a42370f2b66a9a45ec671cb8d95682a69
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-08-03 19:27:37 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+
+  def __init__(self, word):
+    self.word = word.lower()
+    self.sorted_word = sorted(self.word)
+
+  def match(self, word_list):
+    return [
+      new_word for new_word in word_list
+      if sorted(new_word.lower()) == self.sorted_word and new_word.lower() != self.word
+    ]
+
+
